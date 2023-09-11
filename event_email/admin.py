ca3d74d7b80fd182774b43cd7c24f5d5bf99f69f
Nitish Singh <nitishsingh560@gmail.com>
2023-09-11 09:53:39 +0530

    created models, created email sender and handled multiple use cases

--- event_email/admin.py
+++ event_email/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from event_email.models import Event, EmpEvent, EmployeeEmail
+from event_email.models import Event, EmpEvent, EmployeeEmail, EventEmailStatus, EventLog, SystemLog
 # Register your models here.
 
 admin.site.register(Event)
 admin.site.register(EmpEvent)
 admin.site.register(EmployeeEmail)
+admin.site.register(EventEmailStatus)
+admin.site.register(EventLog)
+admin.site.register(SystemLog)
